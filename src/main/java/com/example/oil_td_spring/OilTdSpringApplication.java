824029ac5a7cbdcfc31513fccfae56748b2c29fa
Cr2do <credoay@gmail.com>
2024-12-13 09:16:03 +0100

    init project

---
+++ src/main/java/com/example/oil_td_spring/OilTdSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.oil_td_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilTdSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OilTdSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oil_td_spring/OilTdSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oil_td_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilTdSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
